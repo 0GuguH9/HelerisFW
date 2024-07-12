f39aa121be53d249c65dfa354b205f7e1292885b
0GuguH9 <ghstavohbardinelli@gmail.com>
2024-07-11 21:29:05 -0300

    updated Disclaimer.md and README.md

--- include/heleris/fw/graphics/shader.h
+++ include/heleris/fw/graphics/shader.h
@@ -8,14 +8,34 @@ enum EHRSShaderType {
     HRS_GL_SHADER_FRAGMENT,
 };
 
+/*
+ * Represents an OpenGL shader object.
+ */
 typedef struct HRSShader {
     glObject_t glShader;
     enum EHRSShaderType type;
     char *source;
 } HRSShader;
 
-HRSShader* hrssh_create(enum EHRSShaderType type, char *source);
 
-void hrssh_free(HRSShader *shader);
+/*
+ * Create a shader with a source
+ */
+HRSShader hrssh_create(enum EHRSShaderType type, char *source);
+
+/*
+ * Compile a shader (in this moment you can free the shader source) and set the source to nullptr.
+ */
+void hrssh_compile(HRSShader shader);
+
+/*
+ * Assert the GL shader ins't null.
+ */
+void hrssh_assert(HRSShader shader);
+
+/*
+ * Free a shader
+ */
+void hrssh_free(HRSShader shader);
 
 #endif
